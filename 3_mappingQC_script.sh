07772a7b147e7aa745c74963a2cc8fe0a6be03f1
Kexin Yu <kexinyu@users.noreply.github.com>
2025-10-03 14:58:22 +0100

    updated slurm script and command

---
+++ 3_mappingQC_script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job, 
+## please type
+##
+##    sbatch script_name.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
+
+## Specify a partition. Check available partitions using sinfo Slurm command.
+#SBATCH --partition=short
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=100G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=8
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as DAYS-HOURS:MINUTES:SECONDS. This example is one hour.
+#SBATCH --time=0-01:00:00
+
+## Provide file name (files will be saved in directory where job was ran) or path
+## to capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+
+## ################### CODE TO RUN ##########################
+# Load modules (if required - e.g. when not using conda) 
+# module load R-base/4.3.0
+
+# Execute these commands 
+ samtools view -b /project/wolf7099/linux/2_rnaseq/3_analysis/aln-pe.sam > aln-pe.bam 
+ samtools sort -@ 8 aln-pe.bam > sorted.bam 
+ samtools index sorted.bam 
+ samtools idxstats sorted.bam > sorted.idxstats 
+ samtools flagstat sorted.bam > sorted.flagstat 
+
